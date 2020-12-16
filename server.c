147176196012b41604015065b1d84c8288ec59c1
matixezor <ThreeDaysGrace123>
2020-12-16 21:59:37 +0100

    add fork and func to handle client

--- server.c
+++ server.c
@@ -4,12 +4,17 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <unistd.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
 
 #define buffer_size 1024
+#define output_buffer_size 2000
 #define port 3001
+#define max_clients 50
+
+#define welcome_msg "-----------Welcome-----------\n"
 
 /*void client_handler(int client_socket,int client_addr)
 {
@@ -21,18 +26,73 @@
 }*/
 
 
+typedef struct{
+    struct sockaddr_in address;
+    int id;
+    int socket;
+    char name[32];
+
+} client_struct;
+
+
+client_struct clients[max_clients];
+
+/*
+void init_client(struct client_struct *client, int id, struct sockaddr_in address, int socket, char* name){
+    client->address = address;
+    client->id = id;
+    client->socket = socket;
+    client->name = name;
+}
+*/
+
+void send_to_all(char *msg, int client_id){
+    for(int i = 0; i <= max_clients; i++){
+        if(clients[i].id >= 0){
+            if(clients[i].id != client_id){
+                write(clients[i].socket, msg, strlen(msg));
+            }
+        }
+    }
+}
+
+
+int handle_client(struct client_struct client){
+    char recv_buffer[buffer_size];
+    char output_buffer[output_buffer_size];
+
+    while(1)
+    {
+        memset(recv_buffer, 0, buffer_size);
+        if(recv(client.socket, recv_buffer, buffer_size, 0)<= 0)
+        {
+            memset(output_buffer, 0 ,output_buffer_size);
+            sprintf(output_buffer, "%s disconnected\n", client.name);
+            close(client.socket);
+            printf("%s(%s) disconnected\n", inet_ntoa(client.address), client.name);
+            send_to_all(output_buffer, client.id);
+            break;
+        }
+        printf("%s(%s): %s", inet_ntoa(client.address), client.name, recv_buffer);
+        sprintf(output_buffer, "%s: %s", client.name, recv_buffer);
+        send_to_all(output_buffer, client.id);
+    }
+    return client.id;
+}
+
 
 
 int main()
 {
     int server_socket, new_socket;
-    //char message[buffer_size];
+    int curr_clients_amount = 0;
 
     struct sockaddr_in client_addr;
     struct sockaddr_in server_addr;
     //int client_addrlen = sizeof(client_addr);
     //int server_addrlen = sizeof(server_addr);
     socklen_t addrlen = sizeof(struct sockaddr_in);
+    char recv_name[32];
 
     server_socket = socket(PF_INET, SOCK_STREAM, 0);
     server_addr.sin_family = AF_INET;
@@ -62,37 +122,48 @@ int main()
     {
         socklen_t addrlen = sizeof(struct sockaddr_in);
         new_socket = accept(server_socket, (struct sockaddr*) &client_addr, &addrlen);
+
         if (new_socket < 0)
         {
             printf("Accept failed\n");
             continue;
         }
+
         printf("Connection from %s:%u\n",
             inet_ntoa(client_addr.sin_addr),
             ntohs(client_addr.sin_port)
             );
-        if (fork() == 0)
-        {
-            char recv_buffer[buffer_size];
-            //char send_buffer[buffer_size];
-            while(1)
-            {
-                memset(recv_buffer, 0, buffer_size);
-                if(recv(new_socket, recv_buffer, buffer_size, 0)<= 0)
-                {
-                    printf("%s: disconected", inet_ntoa(client_addr.sin_addr));
-                    break;
-                }
-                printf("%s: %s", inet_ntoa(client_addr.sin_addr), recv_buffer);
-                write(new_socket, recv_buffer, strlen(recv_buffer));
-            }
 
-            exit(0);
+        if (curr_clients_amount > 49){
+            printf("Connection limit reached. Closing connection with %s:%u\n .",
+            inet_ntoa(client_addr.sin_addr),
+            ntohs(client_addr.sin_port)
+            );
+            close(new_socket);
+            continue;
         }
 
+        memset(recv_name, 0, 32);
+        recv(new_socket, recv_name, 32, 0);
+        write(new_socket, welcome_msg, strlen(welcome_msg));
+
+
+        clients[curr_clients_amount].address = client_addr;
+        clients[curr_clients_amount].id = curr_clients_amount;
+        strcpy(clients[curr_clients_amount].name, recv_name);
+        clients[curr_clients_amount].socket = new_socket;
+        curr_clients_amount++;
+
+        if (fork() == 0)
+        {
+            int id;
+            id = handle_client(clients[curr_clients_amount-1]);
+            clients[id] = clients[curr_clients_amount];
+            clients[curr_clients_amount].id = -1;
+            curr_clients_amount--;
+            exit(0);
+        }
     }
-
-
     return 0;
 }
 
